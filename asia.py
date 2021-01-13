bffb4150fca4ce21de866794feea84ccbf712ac8
igor <nextset2005@yandex.ru>
2021-01-13 10:37:09 +0300

    new app

--- asia.py
+++ asia.py
@@ -1,6 +1,102 @@
 from flask import Flask, render_template, redirect, url_for, request, flash
+from flask_login import LoginManager, UserMixin
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import login_user, login_required, logout_user
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask_bootstrap import Bootstrap
+
 
 app = Flask(__name__)
+app.secret_key = 'some secret salt'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:123@localhost/asia'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+manager = LoginManager(app)
+bootstrap = Bootstrap(app)
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    login = db.Column(db.String(128), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False)
+
+
+db.create_all()
+
+
+@app.route('/', methods=['GET'])
+def hello():
+    return render_template('index.html')
+
+
+@app.route('/main', methods=['GET'])
+@login_required
+def main():
+    return render_template('main.html')
+
+
+@manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    login = request.form.get('login')
+    password = request.form.get('password')
+    password2 = request.form.get('password2')
+
+    if request.method == 'POST':
+        if not (login or password or password2):
+            flash('Please, fill all fields!')
+        elif password != password2:
+            flash('Passwords are not equal!')
+        else:
+            hash_pwd = generate_password_hash(password)
+            new_user = User(login=login, password=hash_pwd)
+            db.session.add(new_user)
+            db.session.commit()
+
+            return redirect(url_for('login_page'))
+
+    return render_template('register.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_page():
+    login = request.form.get('login')
+    password = request.form.get('password')
+
+    if login and password:
+        user = User.query.filter_by(login=login).first()
+
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+
+            next_page = request.args.get('next')
+
+            return redirect(next_page)
+        else:
+            flash('Логин или пароль некорректны')
+    else:
+        flash('Пожалуйста введите логин и пароль')
+
+    return render_template('login.html')
+
+
+@app.after_request
+def redirect_to_signin(response):
+    if response.status_code == 401:
+        return redirect(url_for('login_page') + '?next=' + request.url)
+
+    return response
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('hello'))
 
 
 if __name__ == "__main__":
